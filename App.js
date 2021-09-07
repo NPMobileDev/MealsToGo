5738a1665b47f79f11b0cdcfbd7c9659c35bd7ed
Nelson <tomneo2004@gmail.com>
2021-09-07 09:29:32 +0800

    eslint & prettier

--- App.js
+++ App.js
@@ -1,22 +1,37 @@
-import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
 
 export default function App() {
   return (
     <React.Fragment>
-      <SafeAreaView style={{flex:1, marginTop: StatusBar.currentHeight}}>
-        <View style={{backgroundColor:'green', padding:16, justifyContent:'center' }}>
-          <Text >Search</Text>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.Search}>
+          <Text>Search</Text>
         </View>
-        <View style={{flex:1, backgroundColor:'blue', padding:16, justifyContent:'flex-start'}}>
+        <View style={styles.List}>
           <Text>list</Text>
         </View>
       </SafeAreaView>
-      <ExpoStatusBar style='auto' />
+      <ExpoStatusBar style="auto" />
     </React.Fragment>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  Search: {
+    backgroundColor: "green",
+    padding: 16,
+    justifyContent: "center",
+  },
+  List: {
+    flex: 1,
+    backgroundColor: "blue",
+    padding: 16,
+    justifyContent: "flex-start",
+  },
 });
