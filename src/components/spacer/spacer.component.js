9cc8cb46d50e879011382c7b2c2996e1455b8572
Nelson <tomneo2004@gmail.com>
2021-09-15 10:37:25 +0800

    update spacer component

--- src/components/spacer/spacer.component.js
+++ src/components/spacer/spacer.component.js
@@ -2,22 +2,6 @@ import React from "react";
 import styled, { useTheme } from "styled-components/native";
 import { View } from "react-native";
 
-const TopSmall = styled(View)`
-  margin-top: ${(props) => props.theme.space[1]};
-`;
-
-const LeftSmall = styled(View)`
-  margin-left: ${(props) => props.theme.space[1]};
-`;
-
-const LeftMedium = styled(View)`
-  margin-left: ${(props) => props.theme.space[2]};
-`;
-
-const LeftLarge = styled(View)`
-  margin-left: ${(props) => props.theme.space[3]};
-`;
-
 const sizeVariant = {
   small: 1,
   medium: 2,
