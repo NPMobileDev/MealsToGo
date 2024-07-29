74b8ec751cafc3eb4736bda591a6aef4575e3460
Nelson <tomneo2004@gmail.com>
2024-07-29 11:09:27 +0800

    change to production

--- src/utils/env.js
+++ src/utils/env.js
@@ -5,5 +5,5 @@ const localHost = "http://localhost:5001/mealstogo-54005/us-central1";
 
 const isDev = process.env.NODE_ENV === "production";
 export const isAndroid = Platform.OS === "android";
-export const isMock = true;
+export const isMock = false;
 export const host = !isDev || isAndroid ? liveHost : localHost;
